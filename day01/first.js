1e0905d865e042762e91116d463ecd3d8c048061
janakkhadka <janakkhadka08@gmail.com>
2022-11-29 22:30:57 +0545

    day 1 code files completed

---
+++ day01/first.js
@@ -0,0 +1,23 @@
+console.log("hello js")
+console.log(2+3)
+console.log(3**2)//exponentiation 3**2==3*3s
+
+
+console.log(typeof 'janak')
+console.log(typeof 5)
+console.log(typeof true)
+console.log(typeof undefined)
+
+let test=' '
+let fName='Janak'
+let lName='Khadka'
+let country='Nepal'
+let city='Kathmandu'
+let age=22
+let isMarried=false
+
+console.log(test,fName, lName, country, city, age, isMarried)
+
+const gravity=9.8
+const PI=3.14
+console.log(gravity, PI)
