eadd89248bb967fd6bde58eb78d7be66effc41f9
janakkhadka <janakkhadka08@gmail.com>
2022-12-02 21:48:37 +0545

    day 04 exercise

---
+++ day04/day4.js
@@ -0,0 +1,36 @@
+console.log("hello from day 4")
+//excersie 1
+//4.
+/*let a=prompt("Enter any number")
+if(a%2==0)
+{
+    console.log(`${a} is even number`)
+}
+else{
+    console.log(`${a} is odd number`)
+}*/
+//excercise 2
+/*Check if the season is Autumn, Winter, Spring or Summer. If the user input is :
+September, October or November, the season is Autumn.
+December, January or February, the season is Winter.
+March, April or May, the season is Spring
+June, July or August, the season is Summer
+let c=prompt("Enter season")
+c.toLowerCase()
+switch(c)
+{
+    case "autumn":
+    console.log("the month are september october november")
+    break
+    case "winter":
+    console.log("the month are december january february")
+    break
+    case "spring":
+    console.log("the months are march april may")
+    break
+    case "summer":
+    console.log("the month are june july august")
+    break
+    default:
+        console.log("Enter valid season")
+}*/
