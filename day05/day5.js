3a1dd3f1e350b53ad9f94087ec5ba5028836caf9
janakkhadka <janakkhadka08@gmail.com>
2022-12-03 23:26:16 +0545

    day05 half

---
+++ day05/day5.js
@@ -0,0 +1,62 @@
+// //arrays
+// //const arr=Array()
+// //console.log(arr)
+
+
+// arr=[]
+// console.log(arr)
+// const numbers=[0,1,2,3,4,5]
+// console.log(numbers)
+// const fruits=["mango","apple","banana"]
+// console.log(fruits)
+// console.log(fruits.length)
+
+// //different data types in single array
+// const arr1= [
+//     'Asabeneh',
+//     250,
+//     true,
+//     { country: 'Finland', city: 'Helsinki' },
+//     { skills: ['HTML', 'CSS', 'JS', 'React', 'Python'] }
+// ] // arr containing different data types
+// console.log(arr1)
+// //let js = 'JavaScript'
+// // const charsInJavaScript = js.split('')
+
+// // console.log(charsInJavaScript) // ["J", "a", "v", "a", "S", "c", "r", "i", "p", "t"]
+
+// // let companiesString = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+// // const companies = companiesString.split(',')
+
+// // console.log(companies) // ["Facebook", " Google", " Microsoft", " Apple", " IBM", " Oracle", " Amazon"]
+// // let txt =
+// //   'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.'
+// // const words = txt.split(' ')
+
+// // console.log(words)
+// // // the text has special characters think how you can just get only the words
+// // // ["I", "love", "teaching", "and", "empowering", "people.", "I", "teach", "HTML,", "CSS,", "JS,", "React,", "Python"]
+// const arr3='facebook,google,yaahoo,youtube'
+// const arr4=arr3.split(',')
+// console.log(arr4)
+
+
+// console.log(arr3.length-1)
+// console.log(arr4.length-1)
+
+
+const arr = Array() // creates an an empty array
+console.log(arr)
+
+const eightEmptyValues = Array(8) // it creates eight empty values
+console.log(eightEmptyValues) // [empty x 8]
+
+const array5=[1,2,3,4,5]
+console.log(array5.indexOf(5))
+
+
+const numbers1 = [1, 2, 3, 4, 5]
+console.log(numbers1.toString()) // 1,2,3,4,5
+
+const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+console.log(names.toString()) // Asabeneh,Mathias,Elias,Brook
